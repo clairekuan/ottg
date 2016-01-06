70ce9be841edfaea89850e3cf378126301b3777b
Demo User <demouser@n243-96.mtholyoke.edu>
2016-01-06 09:11:06 -0800

    Redirect after POST, and show all items in template.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def enter_a_new_item(self, todo_text):
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(todo_text)
         inputbox.send_keys(Keys.ENTER)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -46,8 +46,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1. Buy peacock feather" as an item in a to-do lists
 
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item
         # She enters 'Use peacock feathers to make fly'
@@ -56,7 +55,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # The homepage updates again, and now shows both its items on her lists
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
         # Edith wonders whether the site will remember her lists.  Then she sees
         # that the site has generated a unique URL for her -- there is some
