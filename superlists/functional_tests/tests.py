e95ba525956b793e3c9e461c62593f9ed65f0101
Demo User <demouser@n243-96.mtholyoke.edu>
2016-01-06 10:33:22 -0800

    Migrated functional tests into own app.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox() #she opens Firefox
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage.
-        self.browser.get('http://localhost:8000') #go to the homepage
+        self.browser.get(self.live_server_url) #go to the homepage
 
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +65,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep.
         self.fail('Finish the app!')
-
-# like main method
-if __name__ == '__main__':
-        unittest.main() #run main test and ignore all warnings
