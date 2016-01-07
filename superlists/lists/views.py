b717fef7bfa6785bbe28bbd964fb64508ace7798
Demo User <demouser@n243-96.mtholyoke.edu>
2016-01-07 08:46:47 -0800

    Associated lists with items.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
+    new_list = List.objects.create()
     #ok not under post request b/c this url is only used for posting data
-    Item.objects.create(text = request.POST['item_text'])
+    Item.objects.create(text = request.POST['item_text'], list = new_list)
     return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
