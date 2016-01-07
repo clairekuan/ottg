512627126035c812fce66e59d4459d32e646e143
Demo User <demouser@n243-96.mtholyoke.edu>
2016-01-07 07:59:13 -0800

    Added a new list URL.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text = request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
+
+def new_list(request):
+    #ok not under post request b/c this url is only used for posting data
+    Item.objects.create(text = request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
     items = Item.objects.all()
-    
-    return render(request, 'list.html', {'items': items,})
+    return render(request, 'list.html', {'items': items, })
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -18,6 +18,7 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'), #url with nothing in it will
+    url(r'^lists/new$', views.new_list, name = 'new_list'),
     #refer to the home page
     url(r'^lists/the-only-list-in-the-world/$', views.view_list, name = 'view_list'),
 ]
