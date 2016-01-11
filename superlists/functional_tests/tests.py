07698d4dd410020db4dd6305824d4e6d90d9c9b4
Demo User <demouser@n242-13.mtholyoke.edu>
2016-01-11 08:16:32 -0800

    Refactored templates to share base.html

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -90,24 +90,24 @@ class NewVisitorTest(LiveServerTestCase):
 
         #Satisfied, they both go back to sleep.
 
-def test_layout_and_styling(self):
-    # Edith has heard about a cool new online to-do app.
-    # She goes to check out its homepage.
-    self.browser.set_window_size(1024, 768)
-    self.browser.get(self.live_server_url) #go to the homepage
+    def test_layout_and_styling(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage.
+        self.browser.set_window_size(1024, 768)
+        self.browser.get(self.live_server_url) #go to the homepage
 
-    #She notices the input box is nicely centered
-    self.check_input_box_is_centered()
+        #She notices the input box is nicely centered
+        self.check_input_box_is_centered()
 
-    #She starts a new list and sees the box is centered
-    self.enter_a_new_item('testing')
-    self.check_input_box_is_centered()
+        #She starts a new list and sees the box is centered
+        self.enter_a_new_item('testing')
+        self.check_input_box_is_centered()
 
 
-def check_input_box_is_centered(self):
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width']/2,
-        512,
-        delta = 5
-    )
+    def check_input_box_is_centered(self):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta = 5
+        )
