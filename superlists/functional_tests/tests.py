462f84a0d1218d5e426b60fd5ef8c0c94a1fc904
Demo User <demouser@n242-13.mtholyoke.edu>
2016-01-12 08:09:25 -0800

    refactored unit tests

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,20 +1,38 @@
-from .base import TodoFunctionalTest
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-#from unittest import skip
+class NewVisitorTest(LiveServerTestCase):
 
-class NewVisitorTest(TodoFunctionalTest):
+    def setUp(self):
+        self.browser = webdriver.Firefox() #she opens Firefox
+        self.browser.implicitly_wait(3) #if anything goes wrong stop after 3 sec
+
+    def tearDown(self):
+        self.browser.quit() #window closes
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def enter_a_new_item(self, todo_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(todo_text)
+        inputbox.send_keys(Keys.ENTER)
+
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage.
         self.browser.get(self.live_server_url) #go to the homepage
 
-
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
         #find h1 tag on the page
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        hearder_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', hearder_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -28,7 +46,7 @@ class NewVisitorTest(TodoFunctionalTest):
         self.enter_a_new_item('Buy peacock feathers')
 
         # When she hits enter, she is taken to a new URL
-        # abd now the page lists "1. Buy peacock feather"
+        # and now the page lists "1. Buy peacock feather"
         # as an item in a to-do lists
         #inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
@@ -43,6 +61,14 @@ class NewVisitorTest(TodoFunctionalTest):
         # The homepage updates again, and now shows both its items on her lists
         self.check_for_row_in_list_table('1. Buy peacock feathers')
         self.check_for_row_in_list_table('2. Use peacock feathers to make fly')
+
+
+
+        ###### Edith deletes the last item on her list
+        #self.delete_a_list_item('2. Use peacock feathers to make fly')
+
+
+
 
         # Now a new user, Francis, comes along to the site
 
