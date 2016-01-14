8f647f72fe6dec26cc24b277ee6a01407495818a
Demo User <demouser@n243-39.mtholyoke.edu>
2016-01-14 10:11:22 -0800

    Can mark items done.

--- superlists/functional_tests/test_toggle_done.py
+++ superlists/functional_tests/test_toggle_done.py
@@ -10,14 +10,24 @@ class ToggleDoneTest(TodoFunctionalTest):
 
         self.browser.find_element_by_id('toggle_done').click()
 
+
     def check_marked_off(self, todo_text):
         row = self.find_table_row(todo_text)
         try:
             row.find_element_by_css_selector('.todo-done')
         except NoSuchElementException:
-            self.fail("%s not marked done!" % (todo_text))
+            self.fail("'%s' not marked done!" % (todo_text))
 
-    def test_can_toggle_finished_items(self):
+
+    def check_not_marked_off(self, todo_text):
+        try:
+            self.check_marked_off(todo_text)
+        except:
+            return
+        self.fail("'%s' not marked done!" % (todo_text))
+
+
+    def test_can_mark_finished_items(self):
         #Edith makes a quick shopping List noticing a checkbox to toggle done items
         self.browser.get(self.live_server_url) #open url
         self.enter_a_new_item('Buy peacock feathers') #enter items
@@ -50,3 +60,39 @@ class ToggleDoneTest(TodoFunctionalTest):
 
         self.toggle_todo_done(['Tie some flys'])
         self.check_marked_off('Tie some flys')
+
+
+    def test_can_mark_finished_items(self):
+        #Edith's ties flying hobby is booming
+        #She wants to use a list that she can use over and over
+        self.browser.get(self.live_server_url)
+        self.enter_a_new_item('Buy feathers')
+        self.enter_a_new_item('Buy fishing line')
+        self.enter_a_new_item('Buy sparkles')
+
+        #She goes to the store and she looks in her closet
+        #she already has fishing line
+        self.toggle_todo_done(['Buy fishing line'])
+        self.check_marked_off('Buy fishing line')
+
+        #She goes to the store and finishes shopping
+        self.toggle_todo_done([
+            'Buy feathers',
+            'Buy sparkles'
+        ])
+
+        self.check_marked_off('Buy feathers')
+        self.check_marked_off('Buy fishing line')
+        self.check_marked_off('Buy sparkles')
+
+
+        #She makes some flys and her closet is empty
+        self.toggle_todo_done([
+            'Buy feathers',
+            'Buy fishing line',
+            'Buy sparkles'
+        ])
+
+        self.check_not_marked_off('Buy feathers')
+        self.check_not_marked_off('Buy fishing line')
+        self.check_not_marked_off('Buy sparkles')
--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -127,7 +127,7 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (current_list.id, ))
         self.assertContains(response, 'input type = "checkbox"')
 
-    def test_POST_items_toggles_done(self):
+    def test_POST_items_marks_done(self):
         #create list and items
         current_list = List.objects.create()
         item1 = Item.objects.create(text = "Item 1", list = current_list)
@@ -178,3 +178,34 @@ class ListViewTest(TestCase):
         item2 = Item.objects.get(id=item2.id)
         self.assertFalse(item1.is_done)
         self.assertFalse(item2.is_done)
+
+    def test_POST_items_toggles_done(self):
+        #create list and items
+        current_list = List.objects.create()
+        item1 = Item.objects.create(
+            text = "Item 1",
+            list = current_list,
+            is_done = True
+        )
+
+        item2 = Item.objects.create(
+            text = "Item 2",
+            list = current_list,
+            is_done = False
+        )
+
+
+        #post data
+        response = self.client.post(
+            '/lists/%d/items/' % (current_list.id),
+            data = {'mark_item_done': [item2.id]}
+        )
+
+        # -including toggle item
+        self.assertRedirects(response, '/lists/%d/' % (current_list.id, ))
+
+        #check that item itself is updated
+        item1 = Item.objects.get(id=item1.id)
+        item2 = Item.objects.get(id=item2.id)
+        self.assertFalse(item1.is_done)
+        self.assertTrue(item2.is_done)
